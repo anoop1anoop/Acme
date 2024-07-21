149d78b9af67bab32576906ada87a9ecd8aa3faa
anoop1anoop <anoop.pant@gmail.com>
2024-07-21 10:09:47 -0400

    Added code for creating new customers

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -6,6 +6,67 @@ import { revalidatePath } from 'next/cache';
 import { redirect } from 'next/navigation';
 import { signIn } from '@/auth';
 import { AuthError } from 'next-auth';
+import { randomUUID } from 'crypto';
+
+const CustomerSchema = z.object({
+  customerId: z.string(),
+  name: z
+    .string({ invalid_type_error: 'Please specify a name', })
+    .min(1, { message: 'Please specify name' }),
+  email: z
+    .string({ invalid_type_error: 'Please specify an email', })
+    .min(1, { message: 'Please specify email' }),
+  image_url: z
+    .string({ invalid_type_error: 'Please specify image url', })
+    .min(1, { message: 'Please specify image' }),
+});
+
+export type CustomerState = {
+  errors?: {
+    name?: string[];
+    email?: string[];
+    image_url?: string[];
+  };
+  message?: string | null;
+};
+
+// Use Zod to update the expected types
+const CreateCustomer = CustomerSchema.omit({ customerId: true });
+
+export async function createCustomer(prevState: CustomerState, formData: FormData) {
+  const validatedFields = CreateCustomer.safeParse({
+    name: formData.get('name'),
+    email: formData.get('email'),
+    image_url: formData.get('image_url'),
+  });
+  console.log('Name');
+  console.log(formData.get('name'));
+
+  // If form validation fails, return errors early. Otherwise, continue.
+  if (!validatedFields.success) {
+    return {
+      errors: validatedFields.error.flatten().fieldErrors,
+      message: 'Provide Missing Fields. Failed to Create Customer.',
+    };
+  }
+
+  // Prepare data for insertion into the database
+  const { name, email, image_url } = validatedFields.data;
+  const customerId = randomUUID();
+  //const urlpath = '//customers//' + image_url;
+
+  /*
+  try {
+    await sql`INSERT INTO customers (id, name, email, image_url) 
+    VALUES (${customerId}, ${name}, ${email}, ${image_url})`;
+  } catch (error) {
+    return { message: 'Database Error: Failed to Create Customer' };
+  }
+*/
+  revalidatePath('/dashboard/customers');
+  redirect('/dashboard/customers');
+
+}
 
 const FormSchema = z.object({
   id: z.string(),
@@ -39,7 +100,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
-    
+
   // If form validation fails, return errors early. Otherwise, continue.
   if (!validatedFields.success) {
     return {
@@ -48,7 +109,7 @@ export async function createInvoice(prevState: State, formData: FormData) {
     };
   }
 
-   
+
   // Prepare data for insertion into the database
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
@@ -79,17 +140,17 @@ export async function updateInvoice(
     amount: formData.get('amount'),
     status: formData.get('status'),
   });
- 
+
   if (!validatedFields.success) {
     return {
       errors: validatedFields.error.flatten().fieldErrors,
       message: 'Please Provide Missing Fields. Failed to Update Invoice.',
     };
   }
- 
+
   const { customerId, amount, status } = validatedFields.data;
   const amountInCents = amount * 100;
- 
+
   try {
     await sql`
       UPDATE invoices
@@ -99,7 +160,7 @@ export async function updateInvoice(
   } catch (error) {
     return { message: 'Database Error: Failed to Update Invoice.' };
   }
- 
+
   revalidatePath('/dashboard/invoices');
   redirect('/dashboard/invoices');
 }
